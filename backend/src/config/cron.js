94fa89c934b867a7a4c3142b316e86751dc5d52f
harshhhh-2-2 <hd2hd2hd2006@gmail.com>
2026-06-10 19:41:09 +0530

    cron jobs updated for ML Service as well

--- backend/src/config/cron.js
+++ backend/src/config/cron.js
@@ -2,13 +2,16 @@ import cron from "cron";
 import https from "https";
 import {ENV} from "./env.js";
 const job = new cron.CronJob("*/14 * * * *", function () {
+  [ENV.API_URL,ENV.ML_SERVICE].forEach((url) =>{
   https
-    .get(ENV.API_URL, (res) => {
+    .get(url, (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully");
       else console.log("GET request failed", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sending request", e));
+  });
 });
+
 
 export default job;
 
